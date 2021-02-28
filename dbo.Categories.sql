e186eb928a101913f177dbf8ec7d57bbb53fe79b
berzerk54 <berzerk54@gmail.com>
2021-02-28 23:38:21 +0700

    Delete dbo.Categories.sql

--- dbo.Categories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Категория] VARCHAR(50) NOT NULL, 
-    [Отдел] VARCHAR(50) NOT NULL, 
-    [ФИО мерча] VARCHAR(50) NOT NULL
-)
